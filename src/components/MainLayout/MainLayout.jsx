f770546af3d011891af376e6b148b8a528f4c8bb
QuachBaoLong <quachbaolong3@gmail.com>
2026-03-14 10:07:22 +0700

    feat: add header footer

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,13 +1,16 @@
 import Header from '@/components/Header/Header'
 import { Outlet } from 'react-router-dom'
+import Footer from '../Footer/Footer'
 
 export default function MainLayout() {
-    return (
-        <>
-            <Header />
-            <main className="pt-16">
-                <Outlet />
-            </main>
-        </>
-    )
+  return (
+    <>
+      <Header />
+
+      <main className="pt-16">
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  )
 }
