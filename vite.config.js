047d5b0530bae3b2e5e8c56bd1adc18c00a8a24c
hoangbmt023 <hoangkutehbcd@gmail.com>
2026-04-09 02:29:26 +0700

    fix cicd Release (#24)
    * Implement chat features and CI testing framework (#18)
    
    * fix: Implement service chatSocket and ui chatbox (#15)
    
    * Feature/fix UI (#17)
    
    * test
    
    * feat: update gitignore
    
    ---------
    
    Co-authored-by: LongTran <tranhoalong2005@gmail.com>
    
    * feat: add testing framework and CI workflow
    
    - Added Vitest for unit testing with scripts in package.json.
    - Implemented tests for tokenService functions.
    - Created a CI workflow for testing and deploying on PR merges to main and release branches.
    - Updated vite.config.js to include test environment configuration.
    - Refactored tokenService.js for improved readability and consistency.
    
    ---------
    
    Co-authored-by: vipdua <tranhoalong2004@gmail.com>
    Co-authored-by: LongTran <tranhoalong2005@gmail.com>
    
    * merge Develop into release (#20)
    
    * fix: Implement service chatSocket and ui chatbox (#15)
    
    * Feature/fix UI (#17)
    
    * test
    
    * feat: update gitignore
    
    ---------
    
    Co-authored-by: LongTran <tranhoalong2005@gmail.com>
    
    * feat: add testing framework and CI workflow
    
    - Added Vitest for unit testing with scripts in package.json.
    - Implemented tests for tokenService functions.
    - Created a CI workflow for testing and deploying on PR merges to main and release branches.
    - Updated vite.config.js to include test environment configuration.
    - Refactored tokenService.js for improved readability and consistency.
    
    * fix: update Vite configuration to include path alias for src directory
    
    * fix: add  alias
    
    * Fix rebase (#19)
    
    * Implement chat features and CI testing framework (#18)
    
    * fix: Implement service chatSocket and ui chatbox (#15)
    
    * Feature/fix UI (#17)
    
    * test
    
    * feat: update gitignore
    
    ---------
    
    Co-authored-by: LongTran <tranhoalong2005@gmail.com>
    
    * feat: add testing framework and CI workflow
    
    - Added Vitest for unit testing with scripts in package.json.
    - Implemented tests for tokenService functions.
    - Created a CI workflow for testing and deploying on PR merges to main and release branches.
    - Updated vite.config.js to include test environment configuration.
    - Refactored tokenService.js for improved readability and consistency.
    
    ---------
    
    Co-authored-by: vipdua <tranhoalong2004@gmail.com>
    Co-authored-by: LongTran <tranhoalong2005@gmail.com>
    
    * fix: Implement service chatSocket and ui chatbox (#15)
    
    * Feature/fix UI (#17)
    
    * test
    
    * feat: update gitignore
    
    ---------
    
    Co-authored-by: LongTran <tranhoalong2005@gmail.com>
    
    * fix: update Vite configuration to include path alias for src directory
    
    * fix: add  alias
    
    ---------
    
    Co-authored-by: vipdua <tranhoalong2004@gmail.com>
    Co-authored-by: LongTran <tranhoalong2005@gmail.com>
    
    ---------
    
    Co-authored-by: vipdua <tranhoalong2004@gmail.com>
    Co-authored-by: LongTran <tranhoalong2005@gmail.com>
    
    * fix: replace sass-embedded with sass
    
    * fix: vite config
    
    ---------
    
    Co-authored-by: vipdua <tranhoalong2004@gmail.com>
    Co-authored-by: LongTran <tranhoalong2005@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,16 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 export default defineConfig({
+  base: "/giencar-ui/", 
+
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
     },
   },
+
   test: {
     environment: "jsdom",
   },
