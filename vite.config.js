e888dcafe864d5d2b5963fdfd8dceab0c6eb99c7
hoangbmt023 <hoangkutehbcd@gmail.com>
2026-04-09 01:54:58 +0700

    merge Develop into release (#20)
    * fix: Implement service chatSocket and ui chatbox (#15)
    
    * Feature/fix UI (#17)
    
    * test
    
    * feat: update gitignore
    
    ---------
    
    Co-authored-by: LongTran <tranhoalong2005@gmail.com>
    
    * feat: add testing framework and CI workflow
    
    - Added Vitest for unit testing with scripts in package.json.
    - Implemented tests for tokenService functions.
    - Created a CI workflow for testing and deploying on PR merges to main and release branches.
    - Updated vite.config.js to include test environment configuration.
    - Refactored tokenService.js for improved readability and consistency.
    
    * fix: update Vite configuration to include path alias for src directory
    
    * fix: add  alias
    
    * Fix rebase (#19)
    
    * Implement chat features and CI testing framework (#18)
    
    * fix: Implement service chatSocket and ui chatbox (#15)
    
    * Feature/fix UI (#17)
    
    * test
    
    * feat: update gitignore
    
    ---------
    
    Co-authored-by: LongTran <tranhoalong2005@gmail.com>
    
    * feat: add testing framework and CI workflow
    
    - Added Vitest for unit testing with scripts in package.json.
    - Implemented tests for tokenService functions.
    - Created a CI workflow for testing and deploying on PR merges to main and release branches.
    - Updated vite.config.js to include test environment configuration.
    - Refactored tokenService.js for improved readability and consistency.
    
    ---------
    
    Co-authored-by: vipdua <tranhoalong2004@gmail.com>
    Co-authored-by: LongTran <tranhoalong2005@gmail.com>
    
    * fix: Implement service chatSocket and ui chatbox (#15)
    
    * Feature/fix UI (#17)
    
    * test
    
    * feat: update gitignore
    
    ---------
    
    Co-authored-by: LongTran <tranhoalong2005@gmail.com>
    
    * fix: update Vite configuration to include path alias for src directory
    
    * fix: add  alias
    
    ---------
    
    Co-authored-by: vipdua <tranhoalong2004@gmail.com>
    Co-authored-by: LongTran <tranhoalong2005@gmail.com>
    
    ---------
    
    Co-authored-by: vipdua <tranhoalong2004@gmail.com>
    Co-authored-by: LongTran <tranhoalong2005@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-        },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
     },
-    define: {
-        global: 'window',
-        'process.env': {}
-    }
-})
+  },
+  test: {
+    environment: "jsdom",
+  },
+});
