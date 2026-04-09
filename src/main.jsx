588147cdbdae3ed358d9edf330dfdea7bfddb291
HuyHoang <hoangkutehbcd@gmail.com>
2026-04-10 00:07:29 +0700

    fix: cicd deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
 window.global = window;
 
-createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-)
+createRoot(document.getElementById("root")).render(
+  // <BrowserRouter>
+  //     <App />
+  // </BrowserRouter>,
+  <HashRouter>
+    <App />
+  </HashRouter>,
+);
